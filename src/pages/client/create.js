bd94705656e0beadd8adac6f885f2b47949fb091
Panem <titoluccao@gmail.com>
2022-07-03 19:40:56 -0300

    feat(pages): style create client page

--- src/pages/client/create.js
+++ src/pages/client/create.js
@@ -1,153 +1,182 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import { useForm } from 'react-hook-form'
 import {
+    Alert,
     TextField,
     FormLabel,
     RadioGroup,
     FormControlLabel,
     Radio,
-    Button,
+    Typography,
 } from '@mui/material'
-import { DatePicker } from '@mui/x-date-pickers'
+import { LoadingButton } from '@mui/lab'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 import Page from '@components/Page'
 import useApi from '@hooks/useApi'
+import formStyles from '@styles/form.module.css'
 
 export default function ClientRegister() {
     const { register, handleSubmit } = useForm()
+    const router = useRouter()
     const [birth, setBirth] = React.useState(null)
-    const [status, setStatus] = React.useState('')
+    const [alert, setAlert] = React.useState({ severity: 'off', message: '' })
     const [loading, setLoading] = React.useState(false)
     const api = useApi()
 
     async function registerClient(client) {
         setLoading(true)
 
-        client.birth = client.birth
-            ? new Date(client.birth).toISOString()
-            : undefined
+        client.birth =
+            client.birth.length !== 0
+                ? new Date(client.birth).toISOString()
+                : undefined
 
         try {
             await api.post('/client', { client })
 
-            document.location.pathname = '/'
+            router.push('/')
         } catch (err) {
-            setStatus('Um erro interno ocorreu')
+            setAlert({ severity: 'error', message: 'Um erro interno ocorreu' })
         }
+
+        setLoading(false)
     }
 
     return (
         <Page title='Cadastrar Cliente'>
-            <form
-                onSubmit={handleSubmit(registerClient)}
+            <main
                 style={{
+                    height: '100vh',
                     display: 'flex',
-                    flexDirection: 'column',
-                    gap: '24px',
+                    justifyContent: 'center',
+                    alignItems: 'center',
                 }}
             >
-                <TextField
-                    id='name'
-                    label='Nome'
-                    {...register('name')}
-                    size='small'
-                    required
-                    autoComplete='off'
-                />
-                <TextField
-                    id='address'
-                    label='Endereço'
-                    {...register('address')}
-                    size='small'
-                    autoComplete='off'
-                />
-                <DatePicker
-                    label='Nascimento'
-                    inputFormat='dd/MM/yyyy'
-                    value={birth}
-                    onChange={setBirth}
-                    renderInput={(params) => (
-                        <TextField
-                            {...params}
-                            {...register('birth')}
-                            id='birth'
-                            size='small'
-                            autoComplete='off'
-                        />
-                    )}
-                />
-                <TextField
-                    id='phone'
-                    type='tel'
-                    label='Telefone'
-                    {...register('phone')}
-                    size='small'
-                    autoComplete='off'
-                />
-                <TextField
-                    id='occupation'
-                    label='Ocupação'
-                    {...register('occupation')}
-                    size='small'
-                    autoComplete='off'
-                />
-                <FormLabel>Gênero</FormLabel>
-                <RadioGroup defaultValue='Masculino' row>
-                    <FormControlLabel
-                        value='Masculino'
-                        control={<Radio />}
-                        label='Masculino'
-                        {...register('gender')}
-                    />
-                    <FormControlLabel
-                        value='Feminino'
-                        control={<Radio />}
-                        label='Feminino'
-                        {...register('gender')}
-                    />
-                </RadioGroup>
-                <FormLabel>Estado Civil</FormLabel>
-                <RadioGroup defaultValue='Solteiro' row>
-                    <FormControlLabel
-                        value='Solteiro'
-                        control={<Radio />}
-                        label='Solteiro(a)'
-                        {...register('maritalState')}
-                    />
-                    <FormControlLabel
-                        value='Casado'
-                        control={<Radio />}
-                        label='Casado(a)'
-                        {...register('maritalState')}
-                    />
-                    <FormControlLabel
-                        value='Separado'
-                        control={<Radio />}
-                        label='Separado(a)'
-                        {...register('maritalState')}
-                    />
-                    <FormControlLabel
-                        value='Divorciado'
-                        control={<Radio />}
-                        label='Divorciado(a)'
-                        {...register('maritalState')}
-                    />
-                    <FormControlLabel
-                        value='Viuvo'
-                        control={<Radio />}
-                        label='Viúvo(a)'
-                        {...register('maritalState')}
-                    />
-                </RadioGroup>
-                {status}
-                <Button
-                    loading={loading}
-                    loadingPosition='start'
-                    startIcon={<SaveIcon />}
-                    variant='contained'
+                <form
+                    className={formStyles.container}
+                    onSubmit={handleSubmit(registerClient)}
                 >
-                    Cadastrar
-                </Button>
-            </form>
+                    {alert.severity !== 'off' && (
+                        <Alert severity={alert.severity}>{alert.message}</Alert>
+                    )}
+                    <Typography
+                        variant='h4'
+                        component='h1'
+                        style={{ marginBottom: '12px' }}
+                    >
+                        Cadastrar
+                    </Typography>
+                    <TextField
+                        id='name'
+                        label='Nome'
+                        {...register('name')}
+                        size='small'
+                        required
+                        autoComplete='off'
+                    />
+                    <TextField
+                        id='address'
+                        label='Endereço'
+                        {...register('address')}
+                        size='small'
+                        autoComplete='off'
+                    />
+                    <DatePicker
+                        label='Nascimento'
+                        inputFormat='dd/MM/yyyy'
+                        value={birth}
+                        onChange={setBirth}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                {...register('birth')}
+                                id='birth'
+                                size='small'
+                                autoComplete='off'
+                            />
+                        )}
+                    />
+                    <TextField
+                        id='phone'
+                        type='tel'
+                        label='Telefone'
+                        {...register('phone')}
+                        size='small'
+                        autoComplete='off'
+                    />
+                    <TextField
+                        id='occupation'
+                        label='Ocupação'
+                        {...register('occupation')}
+                        size='small'
+                        autoComplete='off'
+                    />
+                    <div>
+                        <FormLabel>Gênero</FormLabel>
+                        <RadioGroup defaultValue='Masculino' row>
+                            <FormControlLabel
+                                value='Masculino'
+                                control={<Radio />}
+                                label='Masculino'
+                                {...register('gender')}
+                            />
+                            <FormControlLabel
+                                value='Feminino'
+                                control={<Radio />}
+                                label='Feminino'
+                                {...register('gender')}
+                            />
+                        </RadioGroup>
+                    </div>
+                    <div>
+                        <FormLabel>Estado Civil</FormLabel>
+                        <RadioGroup defaultValue='Solteiro' row>
+                            <FormControlLabel
+                                value='Solteiro'
+                                control={<Radio />}
+                                label='Solteiro(a)'
+                                {...register('maritalState')}
+                            />
+                            <FormControlLabel
+                                value='Casado'
+                                control={<Radio />}
+                                label='Casado(a)'
+                                {...register('maritalState')}
+                            />
+                            <FormControlLabel
+                                value='Separado'
+                                control={<Radio />}
+                                label='Separado(a)'
+                                {...register('maritalState')}
+                            />
+                            <FormControlLabel
+                                value='Divorciado'
+                                control={<Radio />}
+                                label='Divorciado(a)'
+                                {...register('maritalState')}
+                            />
+                            <FormControlLabel
+                                value='Viuvo'
+                                control={<Radio />}
+                                label='Viúvo(a)'
+                                {...register('maritalState')}
+                            />
+                        </RadioGroup>
+                    </div>
+                    <LoadingButton
+                        loading={loading}
+                        loadingPosition='start'
+                        variant='outlined'
+                        type='submit'
+                        style={{
+                            maxWidth: 'fit-content',
+                        }}
+                    >
+                        Cadastrar
+                    </LoadingButton>
+                </form>
+            </main>
         </Page>
     )
 }
