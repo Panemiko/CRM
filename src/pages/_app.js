c619b273923d9e958efb34db232e69911be10ec1
Panem <titoluccao@gmail.com>
2022-07-03 15:35:46 -0300

    feat: add date-fns

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
 import '@styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <Component {...pageProps} />
+        </LocalizationProvider>
+    )
 }
