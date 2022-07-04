c537d8128b5cd2a797100f1a4ba474da8ef654db
Panem <titoluccao@gmail.com>
2022-07-04 14:55:22 -0300

    feat(components): add Modal and ModalWindow

---
+++ src/components/Modal.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+import Modal from '@mui/material/Modal'
+
+export default styled(Modal)`
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    outline: none;
+`
---
+++ src/components/ModalWindow.js
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled'
+
+export default styled.div`
+    padding: 24px 16px;
+    background-color: white;
+    border-radius: 12px;
+    outline: none;
+`
