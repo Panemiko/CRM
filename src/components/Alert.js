e2308da195cd967c51b30918fadc60518c4d6f8f
Panem <titoluccao@gmail.com>
2022-07-05 20:07:45 -0300

    feat(components): add Alert

---
+++ src/components/Alert.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+import Alert from '@mui/material/Alert'
+
+export default styled(Alert)`
+    position: fixed;
+    top: 24px;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 999999;
+`
