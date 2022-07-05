60f2c7bbfc4c1b3acfaa6dc49cce2b1b1ed4321e
Panem <titoluccao@gmail.com>
2022-07-05 20:08:21 -0300

    feat(components): update ModalWindow padding

--- src/components/ModalWindow.js
+++ src/components/ModalWindow.js
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled'
 
 export default styled.div`
-    padding: 24px 16px;
+    padding: 48px;
     background-color: white;
     border-radius: 12px;
     outline: none;
