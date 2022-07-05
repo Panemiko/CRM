979ce92436a31df67969760734f62d35dd91e02f
Panem <titoluccao@gmail.com>
2022-07-05 20:08:41 -0300

    feat(components): add meta description to Page

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Head from 'next/head'
 
 export default function Page(props) {
-    const { title, children } = props
+    const { title, description, children } = props
 
     return (
         <>
             <Head>
                 <title>{title}</title>
+                <meta name='description' content={description} />
             </Head>
             {children}
         </>
