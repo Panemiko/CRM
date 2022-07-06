c782654ffb6b7f27169631eaea6ccd772c3cb0c5
Panem <titoluccao@gmail.com>
2022-07-06 13:27:14 -0300

    feat(components): add ClientsList loading

--- src/components/ClientsList.js
+++ src/components/ClientsList.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import CircularProgress from '@mui/material/CircularProgress'
 import ClientView from '@components/ClientView'
 import useApi from '@hooks/useApi'
 
-const ClientsContainer = styled.div`
+const ClientsContainer = styled.ol`
     height: 80vh;
     border: 1px solid gray;
     border-radius: 8px;
@@ -11,16 +12,22 @@ const ClientsContainer = styled.div`
 `
 
 const LoadingContainer = styled.div`
+    height: 100%;
     display: flex;
+    justify-content: center;
     align-items: center;
 `
 
 export default function ClientsList() {
     const [clients, setClients] = React.useState([])
+    const [loading, setLoading] = React.useState(false)
     const api = useApi()
 
     function getClients() {
+        setLoading(true)
+
         api.get('/client').then((response) => {
+            setLoading(false)
             setClients(response.data.clients)
         })
     }
@@ -29,6 +36,11 @@ export default function ClientsList() {
 
     return (
         <ClientsContainer>
+            {loading && (
+                <LoadingContainer>
+                    <CircularProgress />
+                </LoadingContainer>
+            )}
             {clients.map((client, index) => (
                 <ClientView
                     key={index}
