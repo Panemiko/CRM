b7d134da83a8cad8ee43f40593b7a60ed11da294
Panem <titoluccao@gmail.com>
2022-07-06 13:54:51 -0300

    feat(components): add name Search handler

--- src/components/SearchClients.js
+++ src/components/SearchClients.js
@@ -2,9 +2,22 @@ import React from 'react'
 import TextField from '@mui/material/TextField'
 import IconButton from '@mui/material/IconButton'
 import { MdSearch as SearchIcon } from 'react-icons/md'
+import Context from '@contexts/Context'
 
-export default function SearchClients(props) {
-    const { onSearch } = props
+export default function SearchClients() {
+    const { search } = React.useContext(Context)
+
+    function handleKeyDown(e) {
+        if (e.key === 'Enter') search.search()
+    }
+
+    function updateSearch(e) {
+        search.setFilter({
+            name: {
+                contains: e.target.value,
+            },
+        })
+    }
 
     return (
         <div>
@@ -12,8 +25,10 @@ export default function SearchClients(props) {
                 label='Pesquisar'
                 size='small'
                 style={{ marginRight: '8px' }}
+                onChange={updateSearch}
+                onKeyDown={handleKeyDown}
             />
-            <IconButton color='primary'>
+            <IconButton onClick={search.search} color='primary'>
                 <SearchIcon />
             </IconButton>
         </div>
